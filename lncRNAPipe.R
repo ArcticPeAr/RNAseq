ec832fcd46a367fb7400a3c0c87857767d35a0e5
ArcticPeAr <peterarnesen@protonmail.com>
2022-10-14 15:47:10 +0200

    Get ready to analyse lncRNA and upstream genes

---
+++ lncRNAPipe.R
@@ -0,0 +1,12 @@
+library(rjson)
+
+RNAcentralJson <- fromJSON(file = "/media/petear/SharedPart/mmp_AND_so_rna_type_nameLncRNA_AND_TAXONOMY9606_AND_entry_typeSequence.json")
+
+idVec <- c()
+
+n <- length(RNAcentralJson)
+for (var in 1:n)
+{
+  
+  
+}
