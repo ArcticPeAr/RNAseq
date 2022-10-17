851989d1fb9fd88027a4245a458cd4ea266a1a28
ArcticPeAr <peterarnesen@protonmail.com>
2022-10-17 13:58:37 +0200

    Final commit before big clean up

--- lncRNAPipe.R
+++ lncRNAPipe.R
@@ -2,11 +2,12 @@ library(rjson)
 
 RNAcentralJson <- fromJSON(file = "/media/petear/SharedPart/mmp_AND_so_rna_type_nameLncRNA_AND_TAXONOMY9606_AND_entry_typeSequence.json")
 
-idVec <- c()
+MMPidVec <- c()
 
 n <- length(RNAcentralJson)
+
+#Collecting all ids  
 for (var in 1:n)
 {
-  idVec <- append(idVec, )
-  
+  MMPidVec <- append(idVec, RNAcentralJson[[var]]$id)
 }
