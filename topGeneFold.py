fb3c9b9e1c4fcfd4e00005f331de8a0003d424f4
ArcticPeAr <54914210+ArcticPeAr@users.noreply.github.com>
2023-01-11 13:55:38 +0100

    Function to be imported into compana on request

---
+++ topGeneFold.py
@@ -0,0 +1,83 @@
+#########################################################################################################
+##This script takes a list of up and down regulated genes and uses the fold change to determine the top 150 genes.
+##Also the fold change will be kept in a matrix as "mFC" in the R-package "metaLINCS". This for analysis of perturgagens.  
+#> head(mFC)
+#        Resistant.vs.Sensitive WithaferinA.vs.Untreated UTR.vs.UTS WAR.vs.WAS
+#CDKN2A                   6.200                   -0.036      6.150      6.250
+#CTAG2                    5.655                   -0.232      5.716      5.594
+# HMOX1                    0.602                    6.479      0.669      0.535
+# NLRP11                   4.493                   -0.548      4.318      4.668
+# MBL2                    -4.896                    0.092     -4.850     -4.943
+# SLC43A3                  4.113                   -0.422      4.222      4.003
+#         WAR.vs.UTR WAS.vs.UTS
+# CDKN2A       0.013     -0.086
+# CTAG2       -0.293     -0.170
+# HMOX1        6.412      6.547
+# NLRP11      -0.373     -0.723
+# MBL2         0.046      0.139
+# SLC43A3     -0.531     -0.312
+#########################################################################################################
+
+import pandas as pd
+from biomart import BiomartServer
+
+def changeVersusString(versus):
+    parts = versus.split("-")
+    parts = parts[::-1]
+    result = "_vs_".join(parts)
+    return result
+
+def changeVersusString(versus):
+    parts = versus.split("-")
+    result = "_vs_".join(parts)
+    return result
+
+#From https://autobencoder.com/2021-10-03-gene-conversion/
+def getEnsemblMappings():                                   
+    # Set up connection to server                                               
+    server = BiomartServer("http://useast.ensembl.org/biomart")
+    mart = server.datasets["hsapiens_gene_ensembl"]                            
+                                                                                
+    # List the types of data we want                                            
+    attributes = ["entrezgene_id", "hgnc_symbol" ]
+                                                                                
+    # Get the mapping between the attributes                                    
+    response = mart.search({'attributes': attributes})                          
+    data = response.raw.data.decode('ascii')                                    
+                                                                                
+    entrezID2Name = {}
+    # Store the data in a dict                                                  
+    for line in data.splitlines():
+        line = line.split('\t')
+        # The entries are in the same order as in the `attributes` variable
+        entrezgene_id = line[0]
+        hgnc_symbol = line[1]
+        # Some of these keys may be an empty string. If you want, you can 
+        # avoid having a '' key in your dict by ensuring the 
+        # transcript/gene/peptide ids have a nonzero length before
+        # adding them to the dict
+        entrezID2Name[entrezgene_id] = hgnc_symbol                       
+    return entrezID2Name
+
+getEnsemblMappings()
+
+#function to find Novogenes new csv-files to find the fold change.
+def topGeneFold(versus):
+    #open the files
+    #logFCfile
+    logFCfile = pd.read_feather("/home/petear/Documents/TippyTopGeneDF_ALL.feather")
+    #versus is a string with the comparison. Remove those not part of the versus.
+    logFCfile = logFCfile[logFCfile["Versus"].str.contains(versus) == True]
+    #Change the ENTREZID to string from int
+    logFCfile["ENTREZID"] = logFCfile["ENTREZID"].astype(str)
+    #Add gene name column to logFCfile
+    logFCfile["GeneID"] = logFCfile["ENTREZID"].map(entrezID2Name)
+    #sort the logFCfile by the logFC
+    logFCfile = logFCfile.sort_values(by="logFC", ascending=False)
+    #split the logFCfile in up and down regulated genes
+    upRegDF = logFCfile[logFCfile["logFC"] >= 0]
+    downRegDF = logFCfile[logFCfile["logFC"] < 0]
+return logFCfile, upRegDF, downRegDF
+
+
+
