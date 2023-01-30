048f7f878aac343dbdf1377cec5f14cb5d8fe06a
ArcticPeAr <54914210+ArcticPeAr@users.noreply.github.com>
2023-01-30 16:32:43 +0100

    Added Ensemble and export to excel as two new def

--- topGeneFold.py
+++ topGeneFold.py
@@ -34,11 +34,12 @@ def getEnsemblMappings():
     server = BiomartServer("http://www.ensembl.org/biomart/martservice")
     mart = server.datasets["hsapiens_gene_ensembl"]                            
     # List the types of data we want                                            
-    attributes = ["entrezgene_id", "hgnc_symbol" ]
+    attributes = ["entrezgene_id", "hgnc_symbol", "ensembl_gene_id"]
     # Get the mapping between the attributes                                    
     response = mart.search({'attributes': attributes})                          
     data = response.raw.data.decode('ascii')
     entrezID2Name = {}
+    entrezID2Ensembl = {}
     # Store the data in a dict                                                  
     for line in data.splitlines():
         line = line.split('\t')
@@ -49,16 +50,26 @@ def getEnsemblMappings():
         # avoid having a '' key in your dict by ensuring the 
         # transcript/gene/peptide ids have a nonzero length before
         # adding them to the dict
-        entrezID2Name[entrezgene_id] = hgnc_symbol                       
-    return entrezID2Name
+        entrezID2Name[entrezgene_id] = hgnc_symbol
+    for line in data.splitlines():
+        line = line.split('\t')
+        # The entries are in the same order as in the `attributes` variable
+        entrezgene_id = line[0]
+        ensembl_gene_id = line[2]
+        # Some of these keys may be an empty string. If you want, you can 
+        # avoid having a '' key in your dict by ensuring the 
+        # transcript/gene/peptide ids have a nonzero length before
+        # adding them to the dict
+        entrezID2Ensembl[entrezgene_id] = ensembl_gene_id
+    return entrezID2Name, entrezID2Ensembl 
 
 
 #function to find Novogenes new csv-files to find the fold change.
-def topGeneFold(versus, mapfile):
+def topGeneFoldGeneName(versus, mapfile):
     '''Accepts the versus as string and a file with mapping of and returns the logFCfile, upregulated genes as upRegDF and downregulated genes as downRegDF.'''
     #open the files
     #logFCfile
-    logFCfile = pd.read_feather("/media/veracrypt10/New Folder/Documents/TippyTopGeneDF_ALL.feather")
+    logFCfile = pd.read_feather("/home/petear/Documents/TippyTopGeneDF_ALL.feather")
     #versus is a string with the comparison. Remove those not part of the versus.
     if versus not in logFCfile["Versus"].unique():
         versus = reverseString(versus)
@@ -66,13 +77,59 @@ def topGeneFold(versus, mapfile):
     #Change the ENTREZID to string from int
     logFCfile["ENTREZID"] = logFCfile["ENTREZID"].astype(str)
     #Add gene name column to logFCfile
-    logFCfile["GeneID"] = logFCfile["ENTREZID"].map(mapfile)
+    logFCfile["GeneID"] = logFCfile["ENTREZID"].map(mapfile[0])
     #sort the logFCfile by the logFC
     logFCfile = logFCfile.sort_values(by="logFC", ascending=False)
     #split the logFCfile in up and down regulated genes
     upRegDF = logFCfile[logFCfile["logFC"] >= 0]
     downRegDF = logFCfile[logFCfile["logFC"] < 0]
-    return logFCfile, upRegDF, downRegDF
+    return logFCfile
+
+def topGeneFoldEnsembl(versus, mapfile):
+    '''Accepts the versus as string and a file with mapping of and returns the logFCfile, upregulated genes as upRegDF and downregulated genes as downRegDF.'''
+    #open the files
+    #logFCfile
+    logFCfile = pd.read_feather("/home/petear/Documents/TippyTopGeneDF_ALL.feather")
+    #versus is a string with the comparison. Remove those not part of the versus.
+    if versus not in logFCfile["Versus"].unique():
+        versus = reverseString(versus)
+    logFCfile = logFCfile[logFCfile["Versus"].str.contains(versus) == True]
+    #Change the ENTREZID to string from int
+    logFCfile["ENTREZID"] = logFCfile["ENTREZID"].astype(str)
+    #Add gene name column to logFCfile
+    logFCfile["Ensemble"] = logFCfile["ENTREZID"].map(mapfile[1])
+    #sort the logFCfile by the logFC
+    logFCfile = logFCfile.sort_values(by="logFC", ascending=False)
+    #split the logFCfile in up and down regulated genes
+    upRegDF = logFCfile[logFCfile["logFC"] >= 0]
+    downRegDF = logFCfile[logFCfile["logFC"] < 0]
+    return logFCfile
 
 
 
+def extractJustGenes(DF):
+    '''Accepts a dataframe and returns a list of the genes.'''
+    #Extract the genes
+    genes = DF["GeneID"].tolist()
+    return genes
+
+def ExistInList(dataframe, list):
+    '''Accepts a dataframe and a list and returns a dataframe with the genes in the list.'''
+    #Extract the genes
+    genes = extractJustGenes(dataframe)
+    #Check if the genes are in the list
+    genesInList = []
+    for gene in genes:
+        if gene in list:
+            genesInList.append(gene)
+    #Make a dataframe with the genes in the list
+    genesInListDF = dataframe[dataframe["GeneID"].isin(genesInList)]
+    return genesInListDF
+
+def Export2Excel(dataframe, filename):
+    '''Accepts a dataframe and a filename and exports the dataframe to an excel file.'''
+    #Export the dataframe to an excel file
+    writer = pd.ExcelWriter(filename, engine='xlsxwriter')
+    dataframe.to_excel(writer, sheet_name='Sheet1')
+    writer.save()
+    return
