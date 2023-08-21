927533e36b176cc2800e3b1f2781047ae889872e
ArcticPeAr <54914210+ArcticPeAr@users.noreply.github.com>
2023-08-21 15:15:52 +0200

    Added endycyt and endolys sheets

--- pertMoACounter.R
+++ pertMoACounter.R
@@ -1,48 +1,90 @@
 library(readxl)
 library(writexl)
-library(dplyr)
-
-pertDF <- read_excel("/home/petear/MEGA/TormodGroup/InputData/AllAlgoSi09Aug.xlsx", col_names = FALSE, sheet = 1)
-pertDF <- as.data.frame(pertDF)
-#remove column 2-5 and 7-10
-pertDF <- pertDF[,c(1,6)]
-
-#remove duplicates in ...1
-pertDF <- pertDF %>%
-  distinct(...1, .keep_all = TRUE)
-
-#count each string in ...6
-pertTup <- pertDF %>% group_by(...6) %>% summarise(n = n())
-pertTup <- pertTup[order(pertTup$n, decreasing = TRUE),] 
-
-library(writexl)
-write_xlsx(pertTup, "/home/petear/MEGA/TormodGroup/InputData/MoACounted.xlsx")
-##############################################################################################
+library(openxlsx)
 library(reshape2)
-
-
-# 1. Generate the count dataframe
-count_df <- as.data.frame(table(pertDF$"...6"))
-colnames(count_df) <- c("...6", "n")
-
-# Sort the dataframe by count in descending order
-count_df <- count_df[order(-count_df$n), ]
-
-# Print the count dataframe
-head(count_df, 10)
-
 library(tidyverse)
 
+###########################################################################ALL
+##########################################################################
+
+pertDFAll <- read_excel("/home/petear/MEGA/TormodGroup/InputData/AllAlgoSi09Aug.xlsx", col_names = FALSE, sheet = 1)
+pertDFAll <- as.data.frame(pertDFAll)
+#remove column 2-5 and 7-10
+pertDFAll <- pertDFAll[,c(1,6)]
+
+#remove duplicates in ...1
+pertDFAll <- pertDFAll %>%
+  distinct(...1, .keep_all = TRUE)
+
+# 1. Generate the count dataframe
+countDFAll <- as.data.frame(table(pertDFAll$"...6"))
+colnames(countDFAll) <- c("...6", "n")
+
+# Sort the dataframe by count in descending order
+countDFAll <- countDFAll[order(-countDFAll$n), ]
+
 # Reshape the dataframe
-wide_df <- pertDF %>%
+wideDFAll <- pertDFAll %>%
   group_by(...6) %>%
   mutate(row = row_number()) %>%
   spread(key = ...6, value = ...1) %>%
   select(-row)
 
-head(wide_df)
+
+###########################################################################ECYT
+##########################################################################
+
+pertDFEcyt <- read_excel("/home/petear/MEGA/TormodGroup/InputData/EndocyticAlgoSi09Aug.xlsx", col_names = FALSE, sheet = 1)
+pertDFEcyt <- as.data.frame(pertDFEcyt)
+#remove column 2-5 and 7-10
+pertDFEcyt <- pertDFEcyt[,c(1,6)]
+
+#remove duplicates in ...1
+pertDFEcyt <- pertDFEcyt %>%
+  distinct(...1, .keep_all = TRUE)
+
+# 1. Generate the count dataframe
+countDFEcyt <- as.data.frame(table(pertDFEcyt$"...6"))
+colnames(countDFEcyt) <- c("...6", "n")
+
+# Sort the dataframe by count in descending order
+countDFEcyt <- countDFEcyt[order(-countDFEcyt$n), ]
+
+# Reshape the dataframe
+wideDFEcyt <- pertDFEcyt %>%
+  group_by(...6) %>%
+  mutate(row = row_number()) %>%
+  spread(key = ...6, value = ...1) %>%
+  select(-row)
+
+###########################################################################ELYS
+##########################################################################
+
+pertDFElys <- read_excel("/home/petear/MEGA/TormodGroup/InputData/endolysAlgoSi09Aug.xlsx", col_names = FALSE, sheet = 1)
+pertDFElys <- as.data.frame(pertDFElys)
+#remove column 2-5 and 7-10
+pertDFElys <- pertDFElys[,c(1,6)]
+
+#remove duplicates in ...1
+pertDFElys <- pertDFElys %>%
+  distinct(...1, .keep_all = TRUE)
+
+# 1. Generate the count dataframe
+countDFElys <- as.data.frame(table(pertDFElys$"...6"))
+colnames(countDFElys) <- c("...6", "n")
+
+# Sort the dataframe by count in descending order
+countDFElys <- countDFElys[order(-countDFElys$n), ]
+
+# Reshape the dataframe
+wideDFElys <- pertDFElys %>%
+  group_by(...6) %>%
+  mutate(row = row_number()) %>%
+  spread(key = ...6, value = ...1) %>%
+  select(-row)
 
 
-library(openxlsx)
+###########################################################################Print
+##########################################################################
 
-write.xlsx(list("Counts" = count_df, "Wide Data" = wide_df), "MoACounts.xlsx")
+write.xlsx(list("CountsAll" = countDFAll, "PertsPrMoAAll" = wideDFAll, "CountsEcyt" = countDFEcyt, "PertsPrMoAEcyt" = wideDFEcyt, "CountsElys" = countDFElys, "PertsPrMoAElys" = wideDFElys), "MoACounts.xlsx")
